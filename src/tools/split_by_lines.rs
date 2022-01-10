6572188435ad7aa35f9bd991cd8298785563a308
Skill <dima3000mega@gmail.com>
2022-01-10 22:51:23 +0200

    split by lines save results into its inputs filenames folders

--- src/tools/split_by_lines.rs
+++ src/tools/split_by_lines.rs
@@ -17,7 +17,7 @@ pub struct ByLinesSplitter {
 impl LinesProcessor for ByLinesSplitter {
     fn new(targets: Vec<PathBuf>, results_path: PathBuf, save_period: usize) -> Self {
         let task = Task::SplitByLines;
-
+        
         fn get_lines_n() -> usize {
             let static_err = anyhow::anyhow!("Что-то не так с числом");
             let input = utils::user_input("Количество строк в каждом файле: ");
@@ -59,6 +59,7 @@ impl LinesProcessor for ByLinesSplitter {
     }
 
     fn process(self) -> Result<(), anyhow::Error> {
+        let mut lines_n = self.lines_n;
         println!("Обработка {} файлов", self.targets.len());
 
         let now = time::Instant::now();
@@ -79,12 +80,18 @@ impl LinesProcessor for ByLinesSplitter {
             let lines_count = utils::count_lines(file);
 
             println!(
-                "[{}/{}]Файл: {:?}. Строк: {}",
+                "[{}/{}]Файл: {}. Строк: {}",
                 file_num + 1,
                 self.targets.len(),
-                path,
+                path.display(),
                 lines_count
             );
+
+            lines_n = self.lines_n;
+
+            if self.lines_n > lines_count {
+                lines_n = lines_count;
+            }
 
             let file = match fs::OpenOptions::new().read(true).open(path) {
                 Ok(file) => file,
@@ -100,8 +107,11 @@ impl LinesProcessor for ByLinesSplitter {
             let suffix = self
                 .task
                 .to_suffix()
-                .replace("{num}", &self.lines_n.to_string());
-            let mut results_path = utils::build_results_path(path, &self.results_path, &suffix);
+                .replace("{num}", &lines_n.to_string());
+            let mut self_results_path = self.results_path.clone();
+            self_results_path.push(path.file_name().unwrap_or_default());
+
+            let mut results_path = utils::build_results_path(path, &self_results_path, &suffix);
             let mut results_file = utils::open_results_file(results_path)?;
 
             let mut already_written = 0usize;
@@ -117,25 +127,25 @@ impl LinesProcessor for ByLinesSplitter {
 
                 results.push(combo);
 
-                let next_group = i % self.lines_n == 1 && i > self.lines_n;
+                let next_group = i % lines_n == 1 && i > lines_n;
 
                 let last_combo = lines_count - i == 1;
 
                 // we have the second branch for saving results which we are not going to save right away
                 // so we need to save it only if don't need to switch to the next file
                 // in another way we will be just saving results from the second branch hence results files won't contains needed ammount of lines
-                // UPD: we also need to do self.lines_n - already_written >= self.save_period, because otherwise it write unnecessary data before next_group will be true
+                // UPD: we also need to do lines_n - already_written >= self.save_period, because otherwise it write unnecessary data before next_group will be true
                 let need_save = results.len() == self.save_period
                     && !next_group
-                    && self.lines_n - already_written >= self.save_period;
+                    && lines_n - already_written >= self.save_period;
 
                 if need_save || last_combo {
                     if let Err(e) = utils::save_results(&mut results, &mut results_file) {
                         eprintln!("Couldn't write to file: {}", e);
                     }
                     already_written += self.save_period;
-                } else if next_group && already_written != self.lines_n {
-                    let need_write = self.lines_n - already_written;
+                } else if next_group && already_written != lines_n {
+                    let need_write = lines_n - already_written;
 
                     let mut to_be_written: Vec<&String> = results.iter().take(need_write).collect();
 
@@ -150,8 +160,8 @@ impl LinesProcessor for ByLinesSplitter {
                     let suffix = self
                         .task
                         .to_suffix()
-                        .replace("{num}", &(i - 1 + self.lines_n).to_string());
-                    results_path = utils::build_results_path(path, &self.results_path, &suffix);
+                        .replace("{num}", &(i - 1 + lines_n).to_string());
+                    results_path = utils::build_results_path(path, &self_results_path, &suffix);
                     results_file = utils::open_results_file(results_path)?;
                     already_written = 0;
                 }
